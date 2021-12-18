df62d1c2d324424381f648aaf41cf57bce090861
Kaiwen <wangkaiwen1997@gmail.com>
2021-12-18 20:18:14 +0000

    Restructure analysis
    Cleaned data w.r.t chemotherapy
    structured data according to analysis
    formatted HTML output

--- code/data_preparation.R
+++ code/data_preparation.R
@@ -65,16 +65,31 @@ setnames(df,old='Maintenance therapy after surgery? (Y/N)',new='maintenance_ther
 setnames(df, old = "What neodjuvant did they start with?", new = "type_of_chemo")
 
 col_to_include <- c('DOB','date_of_diagnosis','date_last_follow_up','date_neoadjuvant_chemo_start','date_of_death',
-                    'figo_staging','patient_id',
+                    'figo_staging','patient_id','6_cycle_complete_status',
                     'outcome_after_3_cycles','change_in_chemo','outcome_after_6_cycles',
                     'surgical_outcome','brca_status','performance_status',
-                    'os_time','os_status','pfs_time','pfs_status', "maintenance_therapy", "type_of_chemo")
+                    'os_time','os_status','pfs_time','pfs_status', "maintenance_therapy", "type_of_chemo",'new_chemo_regimen')
 
 clinical_df <- df[,..col_to_include]
 # clinical_df %>% count(type_of_chemo)
+clinical_df[,table(`6_cycle_complete_status`)]
+
 
 # Remove Non-HGSOC cases and patients with no patient_id (they don't have any relevant clinical details)
-clinical_df <- clinical_df[!(patient_id%in%c('R695231','R046570'))]
+# R695231,R046570, R627716, R679420: non-HGSOC
+# R67740,R681384: died after 2 cycles of chemo with no surgery
+# R663500: only patient with performance status of 4. Exclude him for sanity
+# R662523: patient refused any further treatment after one cycle of chemo
+# R662846: patient operable after 6 cycles but refused surgery
+# R666674: patient operable after 6 cycles but refused surgery
+# R652709: patient operable after 4 cycles but refused surgery  
+# R670471: patient operable after 6 cycles but refused surgery
+
+clinical_df <- clinical_df[!(patient_id%in%c('R695231','R046570','R627716','R679420',
+                                             'R677420','R681384',
+                                             'R663500',
+                                             'R662523','R662846','R666674','R652709','R670471'))]
+
 
 clinical_df[,`:=`(date_of_diagnosis=as.Date(date_of_diagnosis),
                   DOB=as.Date(DOB),
@@ -192,8 +207,7 @@ clinical_df[,table(change_in_chemo)]
 
 # Patient who did not have 6 cycles of chemo, change in chemo should be NA
 
-clinical_df[str_detect(change_in_chemo,regex('N/A|NA|no documentation',ignore_case=T)),change_in_chemo:=NA]
-
+clinical_df[str_detect(change_in_chemo,'N/A|NA|no documentation'),change_in_chemo:=NA]
 
 
 clinical_df[str_detect(change_in_chemo,regex('no|N$',ignore_case=T)),change_in_chemo:='no_change']
@@ -213,35 +227,22 @@ clinical_df[, outcome_after_6_cycles:=
                 str_detect(outcome_after_6_cycles, regex("^(?i)yes")) ~"yes"
               )]
 
+###=============================================###
 # patient grouping
+###=============================================###
+
 # patient completed 3 cycles and have surgery
 clinical_df[outcome_after_3_cycles=='yes',patient_group := '3_cycle_surgery']
 
 # patient went on to 6 cycles and continued on carbo/taxol till 6 cycles and have surgery
-clinical_df[outcome_after_3_cycles=='no'&change_in_chemo=='no_change' & outcome_after_6_cycles=='yes',patient_group := '6_cycle_no_chemo_change_surgery']
+clinical_df[outcome_after_3_cycles=='no' & outcome_after_6_cycles=='yes',patient_group := '6_cycle_surgery']
 
-# patient went on to 6 cycles and switched chemo and have surgery
-clinical_df[outcome_after_3_cycles=='no'&change_in_chemo=='changed'&outcome_after_6_cycles=='yes',patient_group := '6_cycle_chemo_change_surgery']
-
-# patient went on to 6 cycles, swithced chemo with no intention to operate 
-clinical_df[outcome_after_3_cycles=='no'&change_in_chemo=='changed'&outcome_after_6_cycles=='no',patient_group := '6_cycle_no_surgery']
+# patient went on to 6 cycles, switched chemo with no intention to operate 
+clinical_df[outcome_after_3_cycles=='no'& outcome_after_6_cycles=='no',patient_group := '6_cycle_no_surgery']
 
 
+clinical_df[is.na(patient_group)]
 
-clinical_df <- clinical_df %>% 
-  mutate(patient_group = 
-           case_when( 
-             patient_group %in% c("6_cycle_chemo_change_surgery", "6_cycle_no_chemo_change_surgery") ~ "6_cycle_surgery",
-             patient_group == "3_cycle_surgery " ~ "3_cycle_surgery ",
-             patient_group == "6_cycle_no_surgery" ~ NA_character_,
-             TRUE ~ patient_group
-           ))
-
-clinical_df[,patient_group:=factor(patient_group)]
-
-clinical_df %>% count(patient_group)
-
-clinical_df[,table(patient_group)]
 
 # figo staging
 clinical_df[,table(figo_staging)]
@@ -259,21 +260,98 @@ clinical_df <- clinical_df %>%
                 str_detect(maintenance_therapy, regex("(?i)no|Nil|Two further cycles|offered radiotherapy")) ~ "no")
   )
 
-clinical_df <- clinical_df %>% 
-  mutate(
-    type_of_chemo_cleaned = 
-      case_when(
-        type_of_chemo %in% c("NA", "N/A", "??") ~ NA_character_,
-        str_detect(type_of_chemo, "(?i)carbo.*pac.*") ~ "1",
-        str_detect(type_of_chemo, "(?i)single.*carbo.*") ~ "2",
-        TRUE ~ "3" 
-      )
-    
-    ### three groups you can find: 
-    ### carboplatin + pac
-    ### carboplatin alone 
-    ### other combinations e.g. Carboplatin + paclitaxel, bevacizumab 
-  )
+
+# Type of chemo used in first attempt
+
+table(df$type_of_chemo)
+
+clinical_df[,bevacizumab:=ifelse(str_detect(type_of_chemo, regex("bevaci|avastin",ignore_case=T)),1,0)]
+clinical_df[,carbo_pac:= ifelse(str_detect(type_of_chemo, regex("(?i)carbo.*(pac.*|taxol.*)",ignore_case=T)),1,0)]
+clinical_df[,carbo:=ifelse(str_detect(type_of_chemo, regex("(carbo.*(?! pac.*|taxol.*)(?! bevaci|avastin))",ignore_case = T)),1,0)]
+
+clinical_df[bevacizumab==1,first_chemo_cleaned:='bevacizumab_based']
+clinical_df[carbo_pac==1&bevacizumab==0,first_chemo_cleaned:='carbo_pac']
+clinical_df[carbo==1&carbo_pac==0&bevacizumab==0,first_chemo_cleaned:='carbo']
+
+
+# The second chemo tried
+
+# clinical_df[,table(new_chemo_regimen)]
+# # 
+# test_df = clinical_df[,.(patient_id, type_of_chemo,new_chemo_regimen,change_in_chemo,outcome_after_3_cycles)]
+# test_df = test_df[str_detect(new_chemo_regimen,'N/A|NA|no documentation',negate=T)]
+
+clinical_df[patient_id=='R677420',new_chemo_cleaned:='carbo_pac']
+# Same treatment with dose reduction
+clinical_df[patient_id=='R675343',new_chemo_cleaned:=NA]
+clinical_df[patient_id=='R677966',new_chemo_cleaned:='carbo_caelyx']
+clinical_df[patient_id=='R678971',new_chemo_cleaned:='carbo_caelyx']
+clinical_df[patient_id=='R691852',new_chemo_cleaned:='carbo_weekly_pac']
+clinical_df[patient_id=='R692311',new_chemo_cleaned:='carbo_weekly_pac']
+clinical_df[patient_id=='R693820',new_chemo_cleaned:=NA]
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R694700',new_chemo_cleaned:=NA]
+# Stopped chemo
+clinical_df[patient_id=='R695907',new_chemo_cleaned:=NA]
+clinical_df[patient_id=='R696128',new_chemo_cleaned:='anti-hormone']
+clinical_df[patient_id=='R697578',new_chemo_cleaned:='carbo']
+clinical_df[patient_id=='R697764',new_chemo_cleaned:='anti-hormone']
+clinical_df[patient_id=='R682544',new_chemo_cleaned:='bevacizumab_based']
+clinical_df[patient_id=='R684651',new_chemo_cleaned:='weekly_pac']
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R688738',new_chemo_cleaned:=NA]
+clinical_df[patient_id=='R689680',new_chemo_cleaned:='carbo']
+clinical_df[patient_id=='R690237',new_chemo_cleaned:='bevacizumab_based']
+clinical_df[patient_id=='R691053',new_chemo_cleaned:='carbo']
+clinical_df[patient_id=='R661457',new_chemo_cleaned:='bevacizumab_based']
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R661725',new_chemo_cleaned:=NA]
+clinical_df[patient_id=='R662425',new_chemo_cleaned:='caelyx']
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R662920',new_chemo_cleaned:=NA]
+clinical_df[patient_id=='R663320',new_chemo_cleaned:='carbo']
+clinical_df[patient_id=='R663663',new_chemo_cleaned:= 'carbo_weekly_pac']
+
+# Count as no change as only switched on cycle 6
+clinical_df[patient_id=='R663924',new_chemo_cleaned:= NA]
+clinical_df[patient_id=='R663948',new_chemo_cleaned:= 'carbo_weekly_pac']
+clinical_df[patient_id=='R667118',new_chemo_cleaned:= 'bevacizumab_based']
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R653099',new_chemo_cleaned:= NA]
+clinical_df[patient_id=='R654622',new_chemo_cleaned:= 'weekly_pac']
+clinical_df[patient_id=='R655205',new_chemo_cleaned:= 'carbo_caelyx']
+# Same treatment with dose reduction
+clinical_df[patient_id=='R656688',new_chemo_cleaned:= NA]
+clinical_df[patient_id=='R657548',new_chemo_cleaned:= 'stopped']
+clinical_df[patient_id=='R658406',new_chemo_cleaned:= 'carbo_weekly_pac']
+
+# Same treatment with dose reduction
+clinical_df[patient_id=='R617108',new_chemo_cleaned:= NA]
+# Stopped chemo
+clinical_df[patient_id=='R644555',new_chemo_cleaned:= NA]
+clinical_df[patient_id=='R645941',new_chemo_cleaned:= 'bevacizumab_based']
+clinical_df[patient_id=='R646605',new_chemo_cleaned:= 'carbo_weekly_pac']
+
+# count as no change as bevacizumab only started from cycle 6
+clinical_df[patient_id=='R647384',new_chemo_cleaned:= NA]
+# Same treatment with dose reduction
+clinical_df[patient_id=='R651189',new_chemo_cleaned:= NA]
+clinical_df[patient_id=='R651919',new_chemo_cleaned:= 'carbo_pac']
+clinical_df[patient_id=='R652422',new_chemo_cleaned:= 'bevacizumab_based']
+clinical_df[patient_id=='R652428',new_chemo_cleaned:= 'carbo_weekly_pac']
+clinical_df[patient_id=='R652619',new_chemo_cleaned:= 'carbo_weekly_pac']
+
+clinical_df[str_detect(new_chemo_regimen,'N/A|NA|no documentation'),new_chemo_cleaned := NA]
+
+clinical_df[!is.na(new_chemo_cleaned) & new_chemo_cleaned!=first_chemo_cleaned,change_in_chemo_new :='changed']
+clinical_df[is.na(change_in_chemo_new)&!(is.na(first_chemo_cleaned)),change_in_chemo_new :='no change']
+test_df <- clinical_df[,.(patient_id,new_chemo_cleaned,first_chemo_cleaned,change_in_chemo_new)]
+# test_new_chemo <- clinical_df[,.(patient_id,new_chemo_regimen,new_chemo_cleaned)]
 
 #===============================================================#
 # Create multi cox clinical df ----
@@ -281,7 +359,7 @@ clinical_df <- clinical_df %>%
 
 multi_cox_cols <- c('patient_id','os_time','os_status','pfs_time','pfs_status',
                     'age_at_diagnosis','brca_status','figo_staging','performance_status',
-                    'surgical_outcome','patient_group','maintenance_therapy', 'change_in_chemo', 'type_of_chemo_cleaned')
+                    'surgical_outcome','patient_group','maintenance_therapy', 'change_in_chemo_new', 'first_chemo_cleaned','new_chemo_cleaned')
 
 clinical_multi_df <- clinical_df[,..multi_cox_cols]
 
